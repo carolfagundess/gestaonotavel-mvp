f54fe22ecf6d655e04da7bcd7bd3faa420794e79
Carol Fagundes <carolinarftk@gmail.com>
2025-08-07 09:33:55 -0300

    configuring environment

---
+++ src/main/java/br/com/gestaonotavel/gestaonotavel/mvp/GestaonotavelMvp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.gestaonotavel.gestaonotavel.mvp;
+
+/**
+ *
+ * @author carol
+ */
+public class GestaonotavelMvp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
